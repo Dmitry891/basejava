10f95bcf6ca9dc00198bc624fd805843743ca489
Dmitry891 <fda891via@gmail.com>
2021-03-17 18:39:06 +0300

    HW1 basejava change3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -31,7 +31,6 @@ public class ArrayStorage {
             if (storage[i].uuid.equals(uuid)) {
                 System.arraycopy(storage, i + 1, storage, i, size - 1);
                 storage[size - 1] = null;
-            } else {
                 break;
             }
         }
