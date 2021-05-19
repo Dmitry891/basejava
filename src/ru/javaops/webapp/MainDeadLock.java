15290aae1bea06af21f8e128f28ea48601f6e698
Dmitry891 <fda891via@gmail.com>
2021-05-19 10:17:09 +0300

    HW11 Lesson11

---
+++ src/ru/javaops/webapp/MainDeadLock.java
@@ -0,0 +1,27 @@
+package ru.javaops.webapp;
+
+public class MainDeadLock {
+    private static final String lock1 = "lock1";
+    private static final String lock2 = "lock2";
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> printDeadLock(lock1, lock2)).start();
+        new Thread(() -> printDeadLock(lock2, lock1)).start();
+    }
+
+    private static void printDeadLock(String lock1, String lock2) {
+        System.out.println(Thread.currentThread().getName() + " thread is trying to capture " + lock1);
+        synchronized (lock1) {
+            System.out.println(Thread.currentThread().getName() + " has captured the object " + lock1);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " thread is trying to capture " + lock2);
+            synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + " has captured the object " + lock2);
+            }
+        }
+    }
+}
